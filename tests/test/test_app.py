84e10ff6501595c19133335c21952892e1338377
Christopher Pierre-Samuel <c.pierre-samuel@hotmail.co.uk>
2022-04-15 23:49:05 +0100

    Working test branch, still needs updates

--- tests/test/test_app.py
+++ tests/test/test_app.py
@@ -1,20 +1,19 @@
+from turtle import title
 from flask_testing import TestCase
 from application import app, db
 from application.models import GameDetails, Games, Users
 from flask import url_for
+
 class TestBase(TestCase):
     def create_app(self):
-        app.config.update(SQLALCHEMY_DATABASE_URI ='sqlite:///test.db',
+        app.config.update(SQLALCHEMY_DATABASE_URI ='sqlite:///',
             DEBUG=True,
             SECRET_KEY ='SUPER_SECRET_KEY',
             WTF_CSRF_ENABLED=False
         )
         return app
     
-    def setup(self):
-        Games()
-        GameDetails()
-        Users()
+    def setUp(self):
         db.create_all()
         user = Users(First_name='Lawrence', Last_name='Ford', User_name='Anonymouse', Password='SpikeL3372', Email='Lawrence@hotmail.co.uk')
         game = Games(Users_id=int(1), GameDetails_id=int(1), Title='Halo', Platform='Xbox', Rating='9', Status='completed')
@@ -27,14 +26,13 @@ class TestBase(TestCase):
         db.session.add(game)
         db.session.add(game2)
         db.session.add(game_details)
-        db.session.add(game_details2)
-        db.commit()
+        db.session.add(game_details2)        
     
     def tearDown(self):
-        db.session.remove()
         db.drop_all()
 
 class TestView(TestBase):
+    
     def test_home_get(self):
         response = self.client.get(url_for('home'))
         self.assertEqual(response.status_code, 200)
@@ -51,13 +49,13 @@ class TestView(TestBase):
         response = self.client.get(url_for('Add'))
         self.assertEqual(response.status_code, 200)
     
-    # def test_read_get(self):
-    #     response = self.client.get(url_for('Read'))
-    #     self.assertEqual(response.status_code, 200)
+    def test_read_get(self):
+        response = self.client.get(url_for('Read'))
+        self.assertEqual(response.status_code, 200)
 
-    # def test_update_get(self):
-    #     response = self.client.get(url_for('Update', Title = 'Halo' ))
-    #     self.assertEqual(response.status_code, 200)
+    def test_update_get(self):
+        response = self.client.get(url_for('Update', Title = 'Halo' ))
+        self.assertEqual(response.status_code, 200)
 
 class TestAdd(TestBase):
     def test_add_user(self):
@@ -86,12 +84,25 @@ class TestAdd(TestBase):
     def test_add_game(self):
         response = self.client.post(
             url_for('Add'),
-            data = dict(Users_id= int(1),
-            GameDetails_id=int(1),
+            data = dict(Users_id= int(3),
+            GameDetails_id= int(3),
             Title = 'Banjo Kazooie',
             Platform = 'Xbox 360',
             Rating= int(6),
             Status = 'plan to play')
          )
+        self.assertEqual(response.status_code, 200)
+        assert GameDetails.query.filter_by(Title= 'Banjo Kazooie').first().id == 3
 
-    
+    def test_add_game(self):
+        response = self.client.post(
+            url_for('Update', Title = 'Halo'),
+            data = dict(Title = 'Gears of War',
+            Platform = 'Xbox 360', 
+            Rating = int(10),
+            Status ='playing',
+            follow_redirect = True)
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Gears of War', response.data)
+        assert Games.query.filter_by(Title= 'Gears of War').first.id() == 3
